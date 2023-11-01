e8748b9048a9a429283b55827653e32190371add
banisys <alighasempoor3@gmail.com>
2023-11-01 12:39:59 +0330

    add tailwind and inertia

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,13 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
+  @vite('resources/css/app.css')
   @vite('resources/js/app.js')
+  @inertiaHead
 </head>
 
 <body>
-  <div id="app">
-    <example />
-  </div>
+  @inertia
 </body>
 
 </html>
