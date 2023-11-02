334399cda6670ca8fa75093f91753779e6dcfb33
banisys <alighasempoor3@gmail.com>
2023-11-02 12:47:36 +0330

    add fonts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,5 +3,83 @@
 </script>
 
 <template>
-    <p>dddd</p>
+    <div class="wheel-sub-container">
+        <div class="wheel-prices">
+            <div id="pr1" class="price">
+                <div class="">
+                    111
+                </div>
+            </div>
+            <div id="pr2" class="price">
+                <div class="">
+                    222
+                </div>
+            </div>
+            <div id="pr3" class="price">
+                <div class="">
+                    333
+                </div>
+            </div>
+            <div id="pr4" class="price">
+                <div class="">
+                    444
+                </div>
+            </div>
+            <div id="pr5" class="price">
+                <div class="">
+                    555
+                </div>
+            </div>
+            <div id="pr6" class="price">
+                <div class="">
+                    666
+                </div>
+            </div>
+            <div id="pr7" class="price">
+                <div class="">
+                    777
+                </div>
+            </div>
+            <div id="pr8" class="price">
+                <div class="">
+                    888
+                </div>
+            </div>
+            <div id="pr9" class="price">
+                <div class="">
+                    999
+                </div>
+            </div>
+            <div id="pr10" class="price">
+                <div class="">
+                    1010
+                </div>
+            </div>
+            <div id="pr11" class="price">
+                <div class="">
+                    1111
+                </div>
+            </div>
+            <div id="pr12" class="price">
+                <div class="">
+                    1212
+                </div>
+            </div>
+            <div id="pr13" class="price">
+                <div class="">
+                    1313
+                </div>
+            </div>
+            <div id="pr14" class="price">
+                <div class="">
+                    1414
+                </div>
+            </div>
+            <div id="pr15" class="price">
+                <div class="">
+                    1515
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
