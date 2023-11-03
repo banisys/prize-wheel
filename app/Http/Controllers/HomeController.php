e3102f17cd80572e63a1689f8f66cd62f4b34d6d
alighasempoor3 <alighasempoor3@gmail.com>
2023-11-03 22:16:12 +0330

    add test component

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,4 +11,9 @@ class HomeController extends Controller
     {
         return Inertia::render('Home');
     }
+
+    public function test()
+    {
+        return Inertia::render('Test');
+    }
 }
